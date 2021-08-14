73e83e5e28beb054b60f15d20ba177b2d8016e75
Hugo <hugoguzman11@gmail.com>
2021-08-14 19:06:11 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import MyMap from './Components/MyMap'
+
+function App() {
+  return (
+    <div className="App">
+      <MyMap />
+    </div>
+  );
+}
+
+export default App;
