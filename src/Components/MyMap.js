06cbae22cf46bab5b7250f1657d4f5d39f598c9e
Hugo <hugoguzman11@gmail.com>
2021-08-15 22:28:16 -0400

    updated tile layer provider

--- src/Components/MyMap.js
+++ src/Components/MyMap.js
@@ -2,7 +2,7 @@ import React from "react";
 import { MapContainer, TileLayer, Marker } from "react-leaflet";
 import harrisBuyouts from './Data/harrisBuyouts.json'
 
-const position = [37, -95]
+const position = [30, -95.3103]
 
 const harrisBuyoutsArray = Array.from(harrisBuyouts);
 
@@ -11,13 +11,13 @@ class MyMap extends React.Component {
   return (
 <MapContainer 
   center={position} 
-  zoom={4} 
+  zoom={9} 
   scrollWheelZoom={false}
   style={{ height: 500, width: "100%" }}>
   
     <TileLayer
-      attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      attribution='Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+      url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}.png"
     />
 {harrisBuyoutsArray.map(buyouts => (
       <Marker 
