e927b8aee83578196683714cdab9cbd80d8b8998
Hugo <hugoguzman11@gmail.com>
2021-08-15 22:54:53 -0400

    got marker clusters to work but only polygons and numbers (no circles)

--- src/Components/MyMap.js
+++ src/Components/MyMap.js
@@ -20,14 +20,14 @@ class MyMap extends React.Component {
       attribution='Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
       url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}.png"
     />
+    <MarkerClusterGroup>
 {harrisBuyoutsArray.map(buyouts => (
-      <MarkerClusterGroup>
-        <Marker 
-        key={buyouts.TAXIDNO}
-        position={[buyouts.lat, buyouts.long]}>
-        </Marker>
-      </MarkerClusterGroup>
+      <Marker 
+      key={buyouts.TAXIDNO}
+      position={[buyouts.lat, buyouts.long]}>
+      </Marker>
 ))}
+    </MarkerClusterGroup>
   </MapContainer>
   );
 }
