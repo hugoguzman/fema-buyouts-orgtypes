880fa0b46a63963ae60d7e278c64d5cdc9bef38c
Hugo <hugoguzman11@gmail.com>
2021-08-17 07:57:12 -0400

    Added basic LayersControl.Overlay with no real functionality

--- src/Components/MyMap.js
+++ src/Components/MyMap.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { MapContainer, TileLayer, CircleMarker, Tooltip } from "react-leaflet";
+import { MapContainer, TileLayer, CircleMarker, Tooltip, LayersControl } from "react-leaflet";
 import MarkerClusterGroup from 'react-leaflet-markercluster';
 import harrisBuyouts from './Data/harrisBuyouts.json'
 
@@ -16,20 +16,24 @@ class MyMap extends React.Component {
   zoom={9} 
   style={{ height: 500, width: "100%" }}>
   
-    <TileLayer
-      attribution='Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
-      url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}.png"
-    />
-    <MarkerClusterGroup>
-{harrisBuyoutsArray.map(buyouts => (
-      <CircleMarker 
-      key={buyouts.TAXIDNO}
-      center={[buyouts.lat, buyouts.long]}
-      position={[buyouts.lat, buyouts.long]}>
-      <Tooltip>Year: {buyouts.year}</Tooltip>
-      </CircleMarker>
-))}
-    </MarkerClusterGroup>
+  <TileLayer
+    attribution='Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+    url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}.png"
+  />
+    <LayersControl position="topright">
+      <LayersControl.Overlay checked name="Buyout Clusters">
+        <MarkerClusterGroup>
+          {harrisBuyoutsArray.map(buyouts => (
+            <CircleMarker 
+            key={buyouts.TAXIDNO}
+            center={[buyouts.lat, buyouts.long]}
+            position={[buyouts.lat, buyouts.long]}>
+            <Tooltip>Year: {buyouts.year}</Tooltip>
+            </CircleMarker>
+          ))}
+        </MarkerClusterGroup>
+      </LayersControl.Overlay>
+    </LayersControl>
   </MapContainer>
   );
 }
