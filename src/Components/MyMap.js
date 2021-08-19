3b907129f5069a7ee358937cd15082df211fccc4
Hugo <hugoguzman11@gmail.com>
2021-08-19 16:01:46 -0400

    rendered county polygons

--- src/Components/MyMap.js
+++ src/Components/MyMap.js
@@ -1,56 +1,79 @@
-import React from "react";
-import { MapContainer, TileLayer, CircleMarker, Tooltip, LayersControl, LayerGroup } from "react-leaflet";
-import MarkerClusterGroup from 'react-leaflet-markercluster';
-import allBuyouts from './Data/allBuyouts.json'
-
+import React, {Component} from "react";
+import { MapContainer, TileLayer, GeoJSON } from "react-leaflet";
+import mapData from './Data/countyBuyouts.json'
 
 const position = [37.1, -95.7]
 
-const allBuyoutsArray = Array.from(allBuyouts);
+class MyMap extends Component {
+  state = {};
 
-class MyMap extends React.Component {
-  render()  {
-  return (
-<MapContainer 
-  center={position} 
-  zoom={4} 
-  style={{ height: 500, width: "100%" }}>
+  componentDidMount() {
+    console.log(mapData);
+  }
   
-  <TileLayer
-    attribution='Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    url="https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.png"
-  />
-    <LayersControl position="topright">
-      <LayersControl.BaseLayer checked name="Buyout Clusters">
-        <MarkerClusterGroup>
-          {allBuyoutsArray.map(buyouts => (
-            <CircleMarker 
-            radius={5}
-            key={buyouts.ID}
-            center={[buyouts.lat, buyouts.long]}
-            position={[buyouts.lat, buyouts.long]}>
-            <Tooltip>Year: {buyouts.Year}<br></br>City: {buyouts.City}</Tooltip>
-            </CircleMarker>
-          ))}
-        </MarkerClusterGroup>
-      </LayersControl.BaseLayer>
-      <LayersControl.BaseLayer name="Buyout Coordinates">
-        <LayerGroup>
-          {allBuyoutsArray.map(buyouts => (
-            <CircleMarker 
-            radius={1}
-            key={buyouts.ID}
-            center={[buyouts.lat, buyouts.long]}
-            position={[buyouts.lat, buyouts.long]}>
-            <Tooltip>Year: {buyouts.Year}<br></br>City: {buyouts.City}</Tooltip>
-            </CircleMarker>
-          ))}
-        </LayerGroup>
-      </LayersControl.BaseLayer>
-    </LayersControl>
-  </MapContainer>
-  );
-}
+  countryStyle = {
+    fillColor: "red",
+    fillOpacity: 1,
+    color: "black",
+    weight: 2,
+  };
+
+  printMesssageToConsole = (event) => {
+    console.log("Clicked");
+  };
+
+  changeCountryColor = (event) => {
+    event.target.setStyle({
+      color: "green",
+      fillColor: this.state.color,
+      fillOpacity: 1,
+    });
+  };
+
+  onEachCountry = (country, layer) => {
+    const countryName = country.properties.ADMIN;
+    console.log(countryName);
+    layer.bindPopup(countryName);
+
+    layer.options.fillOpacity = Math.random(); //0-1 (0.1, 0.2, 0.3)
+    // const colorIndex = Math.floor(Math.random() * this.colors.length);
+    // layer.options.fillColor = this.colors[colorIndex]; //0
+
+    layer.on({
+      click: this.changeCountryColor,
+    });
+  };
+
+  colorChange = (event) => {
+    this.setState({ color: event.target.value });
+  };
+
+  render() {
+    return (
+      <div>
+      <h1 style={{ textAlign: "center" }}>FEMA Buyouts by County</h1>
+      <MapContainer 
+        center={position} 
+        zoom={4} 
+        style={{ height: 500, width: "100%" }}>
+        <TileLayer
+        attribution='Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.png"
+        />
+          <GeoJSON
+            style={this.countryStyle}
+            data={mapData.features}
+            onEachFeature={this.onEachCountry}
+          />
+      </MapContainer>
+      <input
+        type="color"
+        value={this.state.color}
+        onChange={this.colorChange}
+      />
+    </div>
+    );
+  }
 }
 
 export default MyMap;
