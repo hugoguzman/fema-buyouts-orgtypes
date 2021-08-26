8b0a1f974dfb447dcac53ec776c9da8e3b38b1ed
Hugo <hugoguzman11@gmail.com>
2021-08-26 09:44:55 -0400

    rendered muni coordinates with simple marker

--- src/Components/MyMap.js
+++ src/Components/MyMap.js
@@ -2,6 +2,7 @@ import React, {Component} from "react";
 import { MapContainer, TileLayer, GeoJSON, LayersControl } from "react-leaflet";
 import countyData from './Data/countyBuyouts.json'
 import regionData from './Data/regionalBuyouts.json'
+import muniData from './Data/muniBuyouts.json'
 
 
 const position = [37.1, -95.7]
@@ -21,6 +22,13 @@ class MyMap extends Component {
   regionStyle = {
     fillOpacity: 1,
     color: "purple",
+    weight: .5,
+  
+  };
+
+  muniStyle = {
+    fillOpacity: 1,
+    color: "red",
     weight: .5,
   
   };
@@ -92,6 +100,37 @@ class MyMap extends Component {
 
   };
 
+  onEachMuni = (muni, layer) => {
+    function getColor(d) {
+      return d > 50  ? '#dadaeb' :
+             d > 15  ? '#bcbddc' :
+             d > 5   ? '#9e9ac8' :
+             d > 2   ? '#dadaeb' :
+                        '#f2f0f7';
+  }
+    const formatter = new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+      minimumFractionDigits: 0,
+      maximumFractionDigits: 0,
+    });
+    const buyoutCounty = muni.properties.county
+    const buyoutSubgrantee = muni.properties.subgrantee_clean
+    const buyoutState = muni.properties.state
+    const buyoutGrantcount = muni.properties.grantcount
+    const buyoutDollaramount = formatter.format(muni.properties.dollaramount)
+
+    const muniName = "<b>County: </b>" + buyoutCounty 
+    + "<br><b>Municipality: </b>" + buyoutSubgrantee
+    + "<br><b>State: </b>" + buyoutState
+    + "<br><b>Grant Count: </b>" + buyoutGrantcount
+    + "<br><b>Dollar Amount: </b>" + buyoutDollaramount;
+    console.log(muniName);
+    layer.bindPopup(muniName);
+    
+    layer.options.fillColor = getColor(muni.properties.grantcount);
+
+  };
   render() {
     return (
       <div>
@@ -117,6 +156,13 @@ class MyMap extends Component {
                   style={this.regionStyle}
                   data={regionData.features}
                   onEachFeature={this.onEachRegion}
+                />
+          </LayersControl.Overlay>
+          <LayersControl.Overlay checked name="Regional Entities">
+                <GeoJSON
+                  style={this.muniStyle}
+                  data={muniData.features}
+                  onEachFeature={this.onEachMuni}
                 />
           </LayersControl.Overlay>
         </LayersControl>
