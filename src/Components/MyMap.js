3381b7b90410aa8deb259d904a0335a01595069f
Hugo <hugoguzman11@gmail.com>
2021-09-17 16:20:50 -0400

    Add material ui pickers

--- src/Components/MyMap.js
+++ src/Components/MyMap.js
@@ -1,3 +1,4 @@
+import React, { useState} from "react";
 import { MapContainer, TileLayer, GeoJSON, LayersControl } from "react-leaflet";
 import countyData from './Data/countyBuyouts.json'
 import regionData from './Data/regionalBuyouts.json'
@@ -5,10 +6,37 @@ import muniData from './Data/munigeojson.json'
 import tribalData from './Data/tribalgeojson.json'
 import L from 'leaflet';
 import '@fontsource/roboto';
+import { makeStyles } from '@material-ui/core/styles';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+
+const useStyles = makeStyles((theme) => ({
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+}));
 
 const position = [37.1, -95.7]
 
 function MyMap() {
+  const [from,setFrom] = useState(2000);
+  const [to,setTo] = useState(2020);
+  const classes = useStyles();
+
+  const handleFrom = e => {
+    setFrom(e.target.value);
+  }
+
+  const handleTo = e => {
+    setTo(e.target.value);
+  }
 
   const countyStyle = {
     fillOpacity: 1,
@@ -173,7 +201,7 @@ function MyMap() {
       <MapContainer 
         center={position} 
         zoom={4} 
-        style={{ height: 500, width: "100%" }}>
+        style={{ height: 450, width: "100%" }}>
             <TileLayer
               attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
               url="https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}{r}.png"
@@ -211,6 +239,70 @@ function MyMap() {
           </LayersControl.Overlay>
         </LayersControl>
       </MapContainer>
+      <FormControl className={classes.formControl}>
+        <InputLabel id="demo-simple-select-label">From</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={from}
+          onChange={handleFrom}
+        >
+          <MenuItem value={2000}>2000</MenuItem>
+          <MenuItem value={2001}>2001</MenuItem>
+          <MenuItem value={2002}>2002</MenuItem>
+          <MenuItem value={2003}>2003</MenuItem>
+          <MenuItem value={2004}>2004</MenuItem>
+          <MenuItem value={2005}>2005</MenuItem>
+          <MenuItem value={2006}>2006</MenuItem>
+          <MenuItem value={2007}>2007</MenuItem>
+          <MenuItem value={2008}>2008</MenuItem>          
+          <MenuItem value={2009}>2009</MenuItem>
+          <MenuItem value={2010}>2010</MenuItem>          
+          <MenuItem value={2011}>2011</MenuItem>
+          <MenuItem value={2012}>2012</MenuItem>          
+          <MenuItem value={2013}>2013</MenuItem>
+          <MenuItem value={2014}>2014</MenuItem>
+          <MenuItem value={2015}>2015</MenuItem>
+          <MenuItem value={2016}>2016</MenuItem>
+          <MenuItem value={2017}>2017</MenuItem>
+          <MenuItem value={2018}>2018</MenuItem>
+          <MenuItem value={2019}>2019</MenuItem>
+          <MenuItem value={2020}>2020</MenuItem>
+        </Select>
+        <FormHelperText>Select starting year for analysis.</FormHelperText>
+      </FormControl>
+      <FormControl className={classes.formControl}>
+        <InputLabel id="demo-simple-select-label">To</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={to}
+          onChange={handleTo}
+        >
+          <MenuItem value={2000}>2000</MenuItem>
+          <MenuItem value={2001}>2001</MenuItem>
+          <MenuItem value={2002}>2002</MenuItem>
+          <MenuItem value={2003}>2003</MenuItem>
+          <MenuItem value={2004}>2004</MenuItem>
+          <MenuItem value={2005}>2005</MenuItem>
+          <MenuItem value={2006}>2006</MenuItem>
+          <MenuItem value={2007}>2007</MenuItem>
+          <MenuItem value={2008}>2008</MenuItem>          
+          <MenuItem value={2009}>2009</MenuItem>
+          <MenuItem value={2010}>2010</MenuItem>          
+          <MenuItem value={2011}>2011</MenuItem>
+          <MenuItem value={2012}>2012</MenuItem>          
+          <MenuItem value={2013}>2013</MenuItem>
+          <MenuItem value={2014}>2014</MenuItem>
+          <MenuItem value={2015}>2015</MenuItem>
+          <MenuItem value={2016}>2016</MenuItem>
+          <MenuItem value={2017}>2017</MenuItem>
+          <MenuItem value={2018}>2018</MenuItem>
+          <MenuItem value={2019}>2019</MenuItem>
+          <MenuItem value={2020}>2020</MenuItem>
+        </Select>
+        <FormHelperText>Select ending year for analysis.</FormHelperText>
+      </FormControl>
     </div>
     );
   }
