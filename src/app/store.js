96bcb1beb6f1dfa4602f506591e6b5ee269de619
Hugo <hugoguzman11@gmail.com>
2021-11-01 15:20:40 -0400

    created muniCardSlice

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import countyCardReducer from "../Components/countyCardSlice"
+import muniCardReducer from "../Components/muniCardSlice"
 
 export const store = configureStore({
     reducer: {
         filterCounty: countyCardReducer,
+        filteredMuni: muniCardReducer
     },
 });
