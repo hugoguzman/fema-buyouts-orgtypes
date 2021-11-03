9e87f07f8e74743f4537d0944f96dbc853b1b4d7
Hugo <hugoguzman11@gmail.com>
2021-11-03 19:18:49 -0400

    added state, regional, and tribal card slices

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 import countyCardReducer from "../Components/countyCardSlice"
 import muniCardReducer from "../Components/muniCardSlice"
+import stateCardReducer from "../Components/stateCardSlice"
+import regionalCardReducer from "../Components/regionalCardSlice"
+import tribalCardReducer from "../Components/tribalCardSlice"
+
 
 export const store = configureStore({
     reducer: {
         filterCounty: countyCardReducer,
-        filteredMuni: muniCardReducer
+        filteredMuni: muniCardReducer,
+        filteredState: stateCardReducer,
+        filteredRegional: regionalCardReducer,
+        filteredTribal: tribalCardReducer
     },
 });
