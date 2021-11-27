c7bb09522240d49cfbf8874240bc612036113120
Hugo <hugoguzman11@gmail.com>
2021-11-26 21:28:25 -0500

    add search filter and navigation link feature from react router

--- src/routes/counties.js
+++ src/routes/counties.js
@@ -1,8 +1,9 @@
-import { Link, Outlet } from "react-router-dom";
+import { Link, NavLink, Outlet, useSearchParams } from "react-router-dom";
 import { countyData } from "../countyBuyouts2";
 
 export default function Counties() {
   let counties = countyData();
+  let [searchParams, setSearchParams] = useSearchParams();
   return (
     <div style={{ display: "flex" }}>
       <nav
@@ -12,15 +13,38 @@ export default function Counties() {
         }}
       >
         <Link to="/">Home</Link> | {" "}
-        <Link to="/counties">Counties</Link>
-        {counties.map(county => (
-          <Link
-            style={{ display: "block", margin: "1rem 0" }}
+        <Link to="/counties">Counties</Link> {" "}
+        <input
+          value={searchParams.get("filter") || ""}
+          onChange={event => {
+            let filter = event.target.value;
+            if (filter) {
+              setSearchParams({ filter });
+            } else {
+              setSearchParams({});
+            }
+          }}
+        />
+        {counties.filter(county => {
+            let filter = searchParams.get("filter");
+            if (!filter) return true;
+            let name = county.properties.subgrantee_clean.toLowerCase();
+            return name.startsWith(filter.toLowerCase());
+          })
+        .map(county => (
+          <NavLink
+            style={({ isActive }) => {
+              return {
+                display: "block",
+                margin: "1rem 0",
+                color: isActive ? "red" : ""
+              };
+            }}
             to={`/counties/${county.properties.group}`}
             key={county.properties.subgrantee_clean}
           >
             {county.properties.subgrantee_clean}
-          </Link>
+          </NavLink>
         ))}
       </nav>
       <Outlet />
