f047aefeb84a8d6ff10acf3675c7bc0ca76426ce
Hugo <hugoguzman11@gmail.com>
2021-12-01 22:00:52 -0500

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-//import React, { useEffect, useState} from 'react';
+import React, { useEffect, useState} from 'react';
 import './App.css';
 import TopAppBar from './Components/AppBar';
 import { 
@@ -9,24 +9,24 @@ import {
 import Counties from "./routes/counties";
 import County from "./routes/county";
 import MyMap from "./routes/usmap"
-//import ControlModal from './Components/ControlModal';
-//import ZoomModal from './Components/ZoomModal';
+import ControlModal from './Components/ControlModal';
+import ZoomModal from './Components/ZoomModal';
 
 
 function App() {
   
-  //useEffect(() => {
-		//setOpen(true);
-	//}, []);
-  //const [open, setOpen] = useState(false);
-  //const handleOpen = () => setOpen(true);
-  //const handleClose = () => {
-    //setOpen(false);
-    //setOpenChild(true)
-  //}
-  //const [openChild, setOpenChild] = useState(false);
-  //const handleOpenChild = () => setOpenChild(true);
-  //const handleCloseChild = () => setOpenChild(false);
+  useEffect(() => {
+		setOpen(true);
+	}, []);
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => {
+    setOpen(false);
+    setOpenChild(true)
+  }
+  const [openChild, setOpenChild] = useState(false);
+  const handleOpenChild = () => setOpenChild(true);
+  const handleCloseChild = () => setOpenChild(false);
 
 
   return (
@@ -47,8 +47,8 @@ function App() {
       </Route>
       </Route>
     </Routes>
-      {/*<ControlModal handleOpen={handleOpen} handleClose={handleClose} open={open}/>*/}
-      {/*<ZoomModal handleOpen={handleOpenChild} handleClose={handleCloseChild} open={openChild}/>*/}
+      <ControlModal handleOpen={handleOpen} handleClose={handleClose} open={open}/>
+      <ZoomModal handleOpen={handleOpenChild} handleClose={handleCloseChild} open={openChild}/>
     <Outlet />
     <p> Welcome to AdaptationApps.com. This application provides detailed information on over 2,000 U.S. communities that have implemented buyouts utilizing FEMA grant funds. </p>
     </div>
