fa386a15553d6b77e2f5c1813b5a5630bd1b5479
Hugo <hugoguzman11@gmail.com>
2021-12-02 20:34:18 -0500

    added empty leaflet map to all county pages

--- src/routes/county.js
+++ src/routes/county.js
@@ -1,7 +1,11 @@
 import { useParams } from "react-router-dom";
 import { getCounty } from "../countyBuyouts2";
+import {
+	MapContainer,
+	TileLayer,
+} from 'react-leaflet';
 
-
+const position = [-87.9375687,31.1459866]
 export default function County() {
   let params = useParams();
   const formatter = new Intl.NumberFormat('en-US', {
@@ -20,6 +24,16 @@ export default function County() {
     Total Dollar Amount: {formatter.format(county.properties.dollaramount)} <br />
     Number of Properties: {county.properties.propertycount}
   </p>
+  <MapContainer
+								center={position}
+								zoom={4}
+								style={{ height: 390, width: '100%' }}
+							>
+								<TileLayer
+									attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+									url='https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}{r}.png'
+								/>
+  </MapContainer>
 </main>
 );
 }
