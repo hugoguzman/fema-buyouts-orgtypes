5bc9d0731829a778ab738e0bcccbd43adf9cbd0f
Hugo <hugoguzman11@gmail.com>
2021-12-06 22:46:36 -0500

    created filtered mui data grid

--- src/routes/county.js
+++ src/routes/county.js
@@ -1,14 +1,25 @@
 import { useParams } from "react-router-dom";
-import { getCounty } from "../countyBuyouts2";
+import { getCounty } from "../countyBuyouts2"; 
+import { countyGrants } from "../countyGrants";
+import { getGrant } from "../countyGrants";
 import {
 	MapContainer,
 	TileLayer,
 } from 'react-leaflet';
 import { DataGrid } from '@mui/x-data-grid';
 
-
-
 const position = [-87.9375687,31.1459866]
+
+const columns = [
+  { field: 'id', headerName: 'ID', width: 90 },
+  { field: 'subgrantee_clean', headerName: 'Subgrantee', width: 150}
+];
+
+const rows = [
+  { id: 1, subgrantee_clean: 'Snow'},
+  { id: 2, subgrantee_clean: 'Lannister'}
+];
+
 export default function County() {
   let params = useParams();
   const formatter = new Intl.NumberFormat('en-US', {
@@ -19,6 +30,7 @@ export default function County() {
   });
 
   let county = getCounty(parseInt(params.countyId, 10));
+  
   return (
   <main style={{ padding: "1rem" }}>
   <h2>County: {county.properties.subgrantee_clean} ({county.properties.state})</h2>
@@ -27,6 +39,14 @@ export default function County() {
     Total Dollar Amount: {formatter.format(county.properties.dollaramount)} <br />
     Number of Properties: {county.properties.propertycount}
   </p>
+  <DataGrid
+        rows={countyGrants.filter(subgrantee => subgrantee.subgrantee_clean === county.properties.subgrantee_clean)}
+        columns={columns}
+        pageSize={5}
+        rowsPerPageOptions={[5]}
+        checkboxSelection
+        disableSelectionOnClick
+      />
   <MapContainer
 								center={position}
 								zoom={10}
