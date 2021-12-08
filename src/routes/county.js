b26f1064299fb2f7e53d56ccb6183b24f08f0a4e
Alexander538 <88948463+Alexander538@users.noreply.github.com>
2021-12-08 14:30:48 -0500

    merged

--- src/routes/county.js
+++ src/routes/county.js
@@ -1,6 +1,19 @@
 import { useParams } from "react-router-dom";
-import { getCounty } from "../countyBuyouts2";
+import { getCounty } from "../countyBuyouts2"; 
+import { countyGrants } from "../countyGrants";
+import { DataGrid } from '@mui/x-data-grid';
 
+const columns = [
+  { field: 'subgrantee_clean', headerName: 'County', width: 125},
+  { field: 'state', headerName: 'State', width: 125},
+  { field: 'grantclass', headerName: 'Grant Class', width: 125},
+  { field: 'numberOfFinalProperties', headerName: 'Property Count', width: 125},
+  { field: 'projectAmount', headerName: 'Dollar Amount', width: 125},
+  { field: 'programFy', headerName: 'Year', width: 125},
+  { field: 'costSharePercentage', headerName: 'FEMA Cost Share %', width: 125},
+  { field: 'benefitCostRatio', headerName: 'Benefit Cost Ratio', width: 125},
+  { field: 'id', headerName: 'ID', width: 90 },
+]
 
 export default function County() {
   let params = useParams();
@@ -12,14 +25,23 @@ export default function County() {
   });
 
   let county = getCounty(parseInt(params.countyId, 10));
+  
   return (
-  <main style={{ padding: "1rem" }}>
+  <main style={{ padding: "1rem", width: "100%"}}>
   <h2>County: {county.properties.subgrantee_clean} ({county.properties.state})</h2>
   <p>
-    Number of Grants: {county.properties.grantcount} <br />
-    Total Dollar Amount: {formatter.format(county.properties.dollaramount)} <br />
-    Number of Properties: {county.properties.propertycount}
+    <strong>Number of Grants:</strong> {county.properties.grantcount} <br />
+    <strong>Total Dollar Amount:</strong> {formatter.format(county.properties.dollaramount)} <br />
+    <strong>Number of Properties:</strong> {county.properties.propertycount}
   </p>
+  <DataGrid
+        rows={countyGrants.filter(subgrantee => subgrantee.subgrantee_clean === county.properties.subgrantee_clean)}
+        columns={columns}
+        pageSize={100}
+        rowsPerPageOptions={[200]}
+        checkboxSelection
+        disableSelectionOnClick
+      />
 </main>
 );
 }
