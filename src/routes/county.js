b2046a872d736ba14b5aef0b530bc7a01224a6a3
Hugo <hugoguzman11@gmail.com>
2021-12-11 15:01:51 -0500

    experimenting with graphql, appsync, and apollo v3

--- src/routes/county.js
+++ src/routes/county.js
@@ -2,6 +2,10 @@ import { useParams } from "react-router-dom";
 import { getCounty } from "../countyBuyouts2"; 
 import { countyGrants } from "../countyGrants";
 import { DataGrid } from '@mui/x-data-grid';
+import {
+  useQuery,
+  gql
+} from "@apollo/client";
 
 const columns = [
   { field: 'subgrantee_clean', headerName: 'County', width: 125},
