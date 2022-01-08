de6460bfaa8c0902901e4c1d552842ec1864a8c5
Hugo <hugoguzman11@gmail.com>
2022-01-08 15:50:32 -0500

    update api key

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -4,7 +4,7 @@ const awsmobile =  {
     "aws_appsync_graphqlEndpoint": "https://dxaowooaxnfkdb4fnllekzf4ju.appsync-api.us-east-1.amazonaws.com/graphql",
     "aws_appsync_region": "us-east-1",
     "aws_appsync_authenticationType": "API_KEY",
-    "aws_appsync_apiKey": "da2-3pyyapkemvhcjpv3zkzmts4ux4",
+    "aws_appsync_apiKey": "da2-zbzjk4rhrjfd3lu5lkzjgf4bxq",
 };
 
 export default awsmobile;
