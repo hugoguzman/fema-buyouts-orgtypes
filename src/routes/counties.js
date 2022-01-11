b983093aca33fb46372ab979a97fb8e42794720c
Alexander538 <88948463+Alexander538@users.noreply.github.com>
2022-01-11 10:53:08 -0500

    added drawer for counties filter

--- src/routes/counties.js
+++ src/routes/counties.js
@@ -1,89 +1,117 @@
-import { NavLink, Outlet, useSearchParams } from "react-router-dom";
-import {
-  useQuery,
-  gql
-} from "@apollo/client";
-import { Grid } from "@mui/material";
-
+import * as React from 'react';
+import { NavLink, Outlet, useSearchParams } from 'react-router-dom';
+import { useQuery, gql } from '@apollo/client';
+import { Grid, Container } from '@mui/material';
+import Drawer from '@mui/material/Drawer';
+import Button from '@mui/material/Button';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import SearchIcon from '@mui/icons-material/Search';
 
 const COUNTY_BUYOUT_GRANTS = gql`
-query countyBuyoutGrants {
-  listCountybuyoutgrants(limit: 1000) {
-    items {
-      uuid
-      subgrantee_clean
-    }
-  }
-}
+	query countyBuyoutGrants {
+		listCountybuyoutgrants(limit: 1000) {
+			items {
+				uuid
+				subgrantee_clean
+			}
+		}
+	}
 `;
 
-
 export default function Counties() {
-  let [searchParams, setSearchParams] = useSearchParams();
-  const { loading, error, data } = useQuery(COUNTY_BUYOUT_GRANTS);
+	const [state, setState] = React.useState({
+		top: false,
+		left: false,
+		bottom: false,
+		right: false,
+	});
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :(</p>;
+	const toggleDrawer = (anchor, open) => (event) => {
+		if (
+			event.type === 'keydown' &&
+			(event.key === 'Tab' || event.key === 'Shift')
+		) {
+			return;
+		}
 
-  return (
-    <Grid
-      container
-      direction='row'
-      justifyContent='space-between'
-      alignItems='stretch'
-    >
-      <Grid
+		setState({ ...state, [anchor]: open });
+	};
+
+	let [searchParams, setSearchParams] = useSearchParams();
+	const { loading, error, data } = useQuery(COUNTY_BUYOUT_GRANTS);
+
+	if (loading) return <p>Loading...</p>;
+	if (error) return <p>Error :(</p>;
+
+	return (
+		<div>
+			{/* <Grid
       xs={4}
       item
       component='nav'
-      // sx={{borderRight: 'solid 1px', paddingBottom:'100%'}}
-      >
-      {/* <nav
-        style={{
-          borderRight: "solid 1px",
-          padding: "1rem"
-        }}
       > */}
-        <input
-          value={searchParams.get("filter") || ""}
-          onChange={event => {
-            let filter = event.target.value;
-            if (filter) {
-              setSearchParams({ filter });
-            } else {
-              setSearchParams({});
-            }
-          }}
-        />
-        {data.listCountybuyoutgrants.items.filter(county => {
-            let filter = searchParams.get("filter");
-            if (!filter) return true;
-            let name = county.subgrantee_clean.toLowerCase();
-            return name.startsWith(filter.toLowerCase());
-          })
-        .map(county => (
-          <NavLink
-            style={({ isActive }) => {
-              return {
-                display: "block",
-                margin: "1rem 0",
-                color: isActive ? "red" : ""
-              };
-            }}
-            to={`/counties/${county.subgrantee_clean}`}
-            key={county.uuid}
-          >
-            {county.subgrantee_clean}
-          </NavLink>
-        ))}
-      {/* </nav> */}
-      </Grid>
-      <Grid 
-      item
-      xs={8}
-      >
-      <Outlet />
-      </Grid>
-      </Grid>
-  );
+			{/* <Drawer
+        anchor='left'
+        open={toggleDrawer(true)}
+        onClose={toggleDrawer(false)}
+        > */}
+			{['left'].map((anchor) => (
+				<React.Fragment key={anchor}>
+					<Button variant='contained' onClick={toggleDrawer(anchor, true)} sx={{m: 4, p:2}} startIcon={<SearchIcon />}>search counties</Button>
+					<Drawer
+						anchor={anchor}
+						open={state[anchor]}
+						onClose={toggleDrawer(anchor, false)}
+					>
+						<input
+							value={searchParams.get('filter') || ''}
+							onChange={(event) => {
+								let filter = event.target.value;
+								if (filter) {
+									setSearchParams({ filter });
+								} else {
+									setSearchParams({});
+								}
+							}}
+						/>
+						<List>
+							{data.listCountybuyoutgrants.items
+								.filter((county) => {
+									let filter = searchParams.get('filter');
+									if (!filter) return true;
+									let name = county.subgrantee_clean.toLowerCase();
+									return name.startsWith(filter.toLowerCase());
+								})
+								.map((county) => (
+									<ListItem>
+										<NavLink
+											style={({ isActive }) => {
+												return {
+													display: 'block',
+													margin: '1rem 0',
+													color: isActive ? 'red' : '',
+												};
+											}}
+											to={`/counties/${county.subgrantee_clean}`}
+											key={county.uuid}
+										>
+											{county.subgrantee_clean}
+										</NavLink>
+									</ListItem>
+								))}
+						</List>
+						{/* </nav> */}
+						{/* </Grid> */}
+					</Drawer>
+				</React.Fragment>
+			))}
+			<Container >
+				<Outlet />
+			</Container>
+		</div>
+	);
 }
--- src/routes/county.js
+++ src/routes/county.js
@@ -1,125 +1,131 @@
-import { useParams } from "react-router-dom";
+import { useParams } from 'react-router-dom';
 import { DataGrid } from '@mui/x-data-grid';
-import {
-  useQuery,
-  gql
-} from "@apollo/client";
-import { Card, Grid } from "@mui/material";
-
+import { useQuery, gql } from '@apollo/client';
+import { Card, Grid, Container, Box } from '@mui/material';
 
 const columns = [
-  { field: 'subgrantee_clean', headerName: 'County', width: 125},
-  { field: 'state', headerName: 'State', width: 125},
-  { field: 'grantclass', headerName: 'Grant Class', width: 125},
-  { field: 'numberOfFinalProperties', headerName: 'Property Count', width: 125},
-  { field: 'projectAmount', headerName: 'Dollar Amount', width: 125},
-  { field: 'programFy', headerName: 'Year', width: 125},
-  { field: 'costSharePercentage', headerName: 'FEMA Cost Share %', width: 125},
-  { field: 'benefitCostRatio', headerName: 'Benefit Cost Ratio', width: 125},
-  { field: 'id', headerName: 'ID', width: 90 },
-]
+	{ field: 'subgrantee_clean', headerName: 'County', width: 125 },
+	{ field: 'state', headerName: 'State', width: 125 },
+	{ field: 'grantclass', headerName: 'Grant Class', width: 125 },
+	{
+		field: 'numberOfFinalProperties',
+		headerName: 'Property Count',
+		width: 125,
+	},
+	{ field: 'projectAmount', headerName: 'Dollar Amount', width: 125 },
+	{ field: 'programFy', headerName: 'Year', width: 125 },
+	{ field: 'costSharePercentage', headerName: 'FEMA Cost Share %', width: 125 },
+	{ field: 'benefitCostRatio', headerName: 'Benefit Cost Ratio', width: 125 },
+	{ field: 'id', headerName: 'ID', width: 90 },
+];
 
 const COUNTY_GRANTS = gql`
-query countyGrants {
-  listCountygrants(limit: 1000) {
-    items {
-      county
-      uuid
-      id
-      subgrantee_clean
-      state
-      grantclass
-      numberOfFinalProperties
-      projectAmount
-      programFy
-      benefitCostRatio
-      costSharePercentage
-    }
-  }
-}
+	query countyGrants {
+		listCountygrants(limit: 1000) {
+			items {
+				county
+				uuid
+				id
+				subgrantee_clean
+				state
+				grantclass
+				numberOfFinalProperties
+				projectAmount
+				programFy
+				benefitCostRatio
+				costSharePercentage
+			}
+		}
+	}
 `;
 
 const COUNTY_BUYOUT_GRANTS = gql`
-query countyBuyoutGrants {
-  listCountybuyoutgrants(limit: 1000) {
-    items {
-      county
-      uuid
-      subgrantee_clean
-      state
-      grantcount
-      propertycount
-      dollaramount
-    }
-  }
-}
+	query countyBuyoutGrants {
+		listCountybuyoutgrants(limit: 1000) {
+			items {
+				county
+				uuid
+				subgrantee_clean
+				state
+				grantcount
+				propertycount
+				dollaramount
+			}
+		}
+	}
 `;
 
 export default function CountyOverview() {
-  const params = useParams();
-  const formatter = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-    minimumFractionDigits: 0,
-    maximumFractionDigits: 0,
-  });
+	const params = useParams();
+	const formatter = new Intl.NumberFormat('en-US', {
+		style: 'currency',
+		currency: 'USD',
+		minimumFractionDigits: 0,
+		maximumFractionDigits: 0,
+	});
 
-  const { loading, error, data } = useQuery(COUNTY_BUYOUT_GRANTS);
+	const { loading, error, data } = useQuery(COUNTY_BUYOUT_GRANTS);
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :(</p>;
+	if (loading) return <p>Loading...</p>;
+	if (error) return <p>Error :(</p>;
 
-  const subgrantee = data.listCountybuyoutgrants.items.map(subgrantee => subgrantee.subgrantee_clean);
-  const subgranteeMapper = subgrantee.find(item => item === params.countyId);
-  console.log(subgranteeMapper);
-  
-  function getCounty() {
-    return data.listCountybuyoutgrants.items.find(
-    item => item.subgrantee_clean === subgranteeMapper
-  );
-  }
-  
-  let item = getCounty(parseInt(params.invoiceId, 10));
+	const subgrantee = data.listCountybuyoutgrants.items.map(
+		(subgrantee) => subgrantee.subgrantee_clean
+	);
+	const subgranteeMapper = subgrantee.find((item) => item === params.countyId);
+	console.log(subgranteeMapper);
 
-  return (
-  <Grid
-  container
-  direction='column'
-  justifyContent='space-evenly'
-  alignItems='stretch'
-  sx={{}}
-  >
-  <Card sx={{height:'100%'}}>
-  <h2>County: {item.county} ({item.state})</h2>
-  <p>
-    <strong>Number of Grants:</strong> {item.grantcount} <br />
-    <strong>Total Dollar Amount:</strong> {formatter.format(item.dollaramount)} <br />
-    <strong>Number of Properties:</strong> {item.propertycount}
-  </p>
-  <CountyTables />
-  </Card>
-  </Grid>
-);
+	function getCounty() {
+		return data.listCountybuyoutgrants.items.find(
+			(item) => item.subgrantee_clean === subgranteeMapper
+		);
+	}
+
+	let item = getCounty(parseInt(params.invoiceId, 10));
+
+	return (
+	<>
+			<Card>
+				<h2>
+					County: {item.county} ({item.state})
+				</h2>
+				<p>
+					<strong>Number of Grants:</strong> {item.grantcount} <br />
+					<strong>Total Dollar Amount:</strong>{' '}
+					{formatter.format(item.dollaramount)} <br />
+					<strong>Number of Properties:</strong> {item.propertycount}
+				</p>
+        </Card>
+				<CountyTables />
+			
+	</>
+	);
 }
 
 function CountyTables() {
-  const params = useParams();
-  const { loading, error, data } = useQuery(COUNTY_GRANTS);
+	const params = useParams();
+	const { loading, error, data } = useQuery(COUNTY_GRANTS);
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :(</p>;
+	if (loading) return <p>Loading...</p>;
+	if (error) return <p>Error :(</p>;
 
-  const subgrantee = data.listCountygrants.items.map(subgrantee => subgrantee.subgrantee_clean);
-  const subgranteeMapper = subgrantee.find(item => item === params.countyId);
+	const subgrantee = data.listCountygrants.items.map(
+		(subgrantee) => subgrantee.subgrantee_clean
+	);
+	const subgranteeMapper = subgrantee.find((item) => item === params.countyId);
 
-  return (
-  <Grid item xs={12} sx={{ padding: "1rem"}}>
-  <DataGrid
-        autoHeight
-        sx={{ m:2 }}
-        rows={data.listCountygrants.items.filter(subgrantee => subgrantee.subgrantee_clean === subgranteeMapper)}
-        columns={columns}
-      />
-</Grid>
-);
+	return (
+		// <Grid item xs={12} sx={{ padding: "1rem"}}>
+		<Container sx={{height: '1000px'}}>
+			<DataGrid
+				rowHeight={150}
+				sx={{ m: 2 }}
+				rows={data.listCountygrants.items.filter(
+					(subgrantee) => subgrantee.subgrantee_clean === subgranteeMapper
+				)}
+				columns={columns}
+			/>
+		</Container>
+		// </Grid>
+	);
 }
