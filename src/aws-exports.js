3d7bc7f246a0ab991e098759485102fb5868939b
Alexander538 <88948463+Alexander538@users.noreply.github.com>
2022-01-17 16:14:54 -0500

    fixed graphql error

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -23,7 +23,11 @@ const awsmobile = {
     },
     "aws_cognito_verification_mechanisms": [
         "EMAIL"
-    ]
+    ],
+    "aws_appsync_graphqlEndpoint": "https://dxaowooaxnfkdb4fnllekzf4ju.appsync-api.us-east-1.amazonaws.com/graphql",
+    "aws_appsync_region": "us-east-1",
+    "aws_appsync_authenticationType": "API_KEY",
+    "aws_appsync_apiKey": "da2-zbzjk4rhrjfd3lu5lkzjgf4bxq"
 };
 
 
